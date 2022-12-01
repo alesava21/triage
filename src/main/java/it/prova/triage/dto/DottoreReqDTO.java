d8edf63aee9b5eea9b146300a80a72b840907ecc
System Administrator <root@MacBook-Air-di-Alessandro.local>
2022-12-01 20:37:31 +0100

    implementata la request e la response di dottore

---
+++ src/main/java/it/prova/triage/dto/DottoreReqDTO.java
@@ -0,0 +1,25 @@
+package it.prova.triage.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DottoreReqDTO {
+
+	private String nome;
+	private String cognome;
+	private String codiceDottore;
+	private boolean inVisita;
+	private boolean inServizio;
+
+}
---
+++ src/main/java/it/prova/triage/dto/DottoreRespDTO.java
@@ -0,0 +1,23 @@
+package it.prova.triage.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DottoreRespDTO {
+	
+	private String nome;
+	private String cognome;
+	private String codiceFiscale;
+
+}
